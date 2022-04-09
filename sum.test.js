0df871975ac5d61ec67ae65f87bcc5a8ce01126a
Intan Amalia <intan.amalia@bukalapak.com>
2022-04-09 14:55:23 +0700

    get post patch delete

--- sum.test.js
+++ sum.test.js
@@ -12,10 +12,37 @@ const request = require('supertest');
 const baseUrl = 'https://reqres.in/api';
 
 describe('Todos endpoint', () => {
-	it('should return a 200 status code', async () => {
+	it('GET should return a 200 status code', async () => {
 		const response = await request(baseUrl).get('/users?page=2');
 
     console.log(response.body)
 		expect(response.statusCode).toBe(200);
+	});
+
+	it('POST should return a 201 status code', async () => {
+		const response = await request(baseUrl).post('/users').send({
+      "name": "morpheus",
+      "job": "leader"
+  });
+
+    console.log(response.body)
+		expect(response.statusCode).toBe(201);
+	});
+
+  it('PATCH should return a 200 status code', async () => {
+		const response = await request(baseUrl).patch('/users').send({
+      "name": "morpheus",
+      "job": "zion resident"
+  });
+
+    console.log(response.body)
+		expect(response.statusCode).toBe(200);
+	});
+
+  it('DELETE should return a 204 status code', async () => {
+		const response = await request(baseUrl).delete('/users/2')
+
+    console.log(response.body)
+		expect(response.statusCode).toBe(204);
 	});
 });
